c5664b8b3f388cc2906eef93e6655b95b741904d
Patricio <pechagueb@gmail.com>
2025-09-05 09:12:30 +0100

    Getters, setters

--- src/main/java/com/persistencia/modelo/Cliente.java
+++ src/main/java/com/persistencia/modelo/Cliente.java
@@ -1,5 +1,57 @@
 package com.persistencia.modelo;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class Cliente {
+
+    private String id; //UUID en texto
+    private String nombre;
+    private String email;
+
+    public Cliente() {} // Necesario para Jackson
+
+    public Cliente(String nombre, String email) {
+
+        this.id = UUID.randomUUID().toString();
+        this.nombre = nombre;
+        this.email = email;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+
+        if (!(o instanceof Cliente)) return false;
+
+        Cliente c = (Cliente) o;
+
+        return Objects.equals(id, c.id);
+    }
 
 }
